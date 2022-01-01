62463a1bab6cad9acb9a43bdb1d99261ca866992
Technomystic <lost.trust@yahoo.co.in>
2022-01-01 22:48:41 +0530

    Added Blank Project

---
+++ ProblemOfTheDay/ProblemOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProblemOfTheDay
+//
+//  Created by Niraj on 01/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

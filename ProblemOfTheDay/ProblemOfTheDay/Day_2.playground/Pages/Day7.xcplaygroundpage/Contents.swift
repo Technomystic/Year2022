651ee10a302e521d7de8df3176feec507e079a86
Technomystic <lost.trust@yahoo.co.in>
2022-01-07 23:26:18 +0530

    Day 7

---
+++ ProblemOfTheDay/ProblemOfTheDay/Day_2.playground/Pages/Day7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// https://leetcode.com/problems/linked-list-random-node/
+
+/*
+ Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+ Implement the Solution class:
+
+ Solution(ListNode head) Initializes the object with the integer array nums.
+ int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be choosen.
+
+ Input
+ ["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+ [[[1, 2, 3]], [], [], [], [], []]
+ Output
+ [null, 1, 3, 2, 2, 3]
+
+ Explanation
+ Solution solution = new Solution([1, 2, 3]);
+ solution.getRandom(); // return 1
+ solution.getRandom(); // return 3
+ solution.getRandom(); // return 2
+ solution.getRandom(); // return 2
+ solution.getRandom(); // return 3
+ // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ */
+
+
+ // Definition for singly-linked list.
+// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
+class Solution {
+
+    var head: ListNode?
+    var current: ListNode?
+    var count = 0
+
+    init(_ head: ListNode) {
+        self.head = head
+        self.current = head
+        while self.current != nil {
+            count += 1
+            self.current = current?.next
+        }
+        current = head
+    }
+
+
+    func getRandom() -> Int {
+        var random: Int = Int.random(in: 0..<count)
+        for i in 0..<random {
+            current = current?.next
+            if current == nil {
+                current = head
+            }
+        }
+        return current!.val
+    }
+}

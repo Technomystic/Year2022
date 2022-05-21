dc6aed6c153852ec122634946d52c31565006d58
Technomystic <lost.trust@yahoo.co.in>
2022-05-21 10:34:38 +0530

    Day 140 - 63. Unique Paths II

--- ProblemOfTheDay/ProblemOfTheDay/Day_2.playground/Pages/Day140.xcplaygroundpage/Contents.swift
+++ ProblemOfTheDay/ProblemOfTheDay/Day_2.playground/Pages/Day140.xcplaygroundpage/Contents.swift
@@ -4,56 +4,40 @@ import Foundation
 
 class Solution {
 
-    // 62. Unique Paths
+    // 63. Unique Paths II
     // A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
     // The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
-    // How many possible unique paths are there?
-    // Finds how many unique paths exist.
-    // - Parameters:
-    //   - m: Number of rows.
-    //   - n: Number of columns.
-    // - Returns: Number of unique paths.
+    // Now consider if some obstacles are added to the grids. How many unique paths would there be?
+    // An obstacle and space is marked as 1 and 0 respectively in the grid.
     // Example 1:
-    // Input: m = 3, n = 7
-    // Output: 28
+    // Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+    // Output: 2
+    // Explanation: There is one obstacle in the middle of the 3x3 grid above.
+    // There are two ways to reach the bottom-right corner:
+    // 1. Right -> Right -> Down -> Down
+    // 2. Down -> Down -> Right -> Right
     // Example 2:
-    // Input: m = 3, n = 2
-    // Output: 3
-    // Explanation:
-    // From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
-    // 1. Right -> Down -> Down
-    // 2. Down -> Down -> Right
-    // 3. Down -> Right -> Down
-    // Example 3:
-    // Input: m = 7, n = 3
-    // Output: 28
-    // Example 4:
-    // Input: m = 3, n = 3
-    // Output: 6
+    // Input: obstacleGrid = [[0,1],[0,0]]
+    // Output: 1
 
     // Constraints:
+    // m == obstacleGrid.length
+    // n == obstacleGrid[i].length
     // 1 <= m, n <= 100
-    // It's guaranteed that the answer will be less than or equal to 2 * 10^9.
-    // - Complexity:
-    //   - time: O(n * m), where n is the number of rows, and m is the number of columns.
-    //   - space: O(min(n, m)), where n is the number of rows, and m is the number of columns.
+    // obstacleGrid[i][j] is 0 or 1.
+    func uniquePathsWithObstacles(_ obstacleGrid: [[Int]]) -> Int {
+        var matrix: [[Int]] = Array(repeating: Array(repeating: 0, count: obstacleGrid[0].count), count: obstacleGrid.count)
 
-    func uniquePaths(_ m: Int, _ n: Int) -> Int {
-        var matrix: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
-
-        for i in 0..<n {
-            for j in 0..<m {
-                if i == 0 || j == 0 {
-                    matrix[i][j] = 1
-                } else {
-                    matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]
-                }
+        matrix[0][0] = 1
+        for i in 0..<obstacleGrid.count {
+            for j in 0..<obstacleGrid[0].count {
+                if i > 0 { matrix[i][j] += matrix[i - 1][j] }
+                if j > 0 { matrix[i][j] += matrix[i][j - 1] }
+                if obstacleGrid[i][j] == 1 { matrix[i][j] = 0 }
             }
         }
 
-        return matrix[n - 1][m - 1]
+        return matrix[obstacleGrid.count - 1][obstacleGrid[0].count - 1]
     }
 
 }
-
-//: [Next](@next)
